38f8e2a894d4589c1569cfc2b1389c2f2dc80266
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2017-06-22 16:30:37 +0600

    updated codecov

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setup(name='bangla',
       classifiers=[
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.2',
 		'Programming Language :: Python :: 3.3',
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
