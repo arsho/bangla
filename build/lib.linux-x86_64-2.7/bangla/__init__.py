dc01aac0986372c20d3f89a56999e00039f5ef79
hmtanbir <tanbir2025@gmail.com>
2018-02-21 16:09:14 +0600

    build is done

--- build/lib.linux-x86_64-2.7/bangla/__init__.py
+++ build/lib.linux-x86_64-2.7/bangla/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import datetime
 
+def convert_greg_month_name(greg_month_number):
+    greg_months = [ 'জানুয়ারী', 'ফেব্রুয়ারী', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'আগস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর' ]
+    return greg_months[greg_month_number -1]
+
 bangla_number = ['০', '১','২', '৩','৪',
                  '৫','৬', '৭', '৮', '৯']
 
